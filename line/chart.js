062a09fe139ddbde41ace00b2ce21a66569f6b95
Romain Castillon <castillonromain01@gmail.com>
2019-03-14 11:20:11 +0100

    modif variables

--- line/chart.js
+++ line/chart.js
@@ -1,43 +1,43 @@
 const ctx = document.getElementById("myChart").getContext('2d');
+
 const type_myChart = 'line'
-const labels_myChart = ["%Secteur1", "%Secteur2", "%Secteur3"]
-const data_myChart = [54.6, 44.4, 1]
-const label_myChart = 'IRIS'
+
+const data = {
+  labels: ["%Secteur1", "%Secteur2", "%Secteur3"],
+  datasets: [{
+    fill: false,
+    label: 'IRIS',
+    data: [54.6, 44.4, 1],
+    backgroundColor: [
+      'rgba(255, 99, 132, 0.2)',
+      'rgba(54, 162, 235, 0.2)',
+      'rgba(255, 206, 86, 0.2)',
+    ],
+    borderColor: [
+      'rgba(255,99,132,1)',
+      'rgba(54, 162, 235, 1)',
+      'rgba(255, 206, 86, 1)',
+    ],
+    borderWidth: 1
+  }]
+}
+
+const options = {
+  scales: {
+    yAxes: [{
+      ticks: {
+        beginAtZero: true
+      }
+    }]
+  }
+}
 
 // console.log(ctx)
 // ctx.canvas.height = 100
 
-const backgroundColor_myChart = [
-  'rgba(255, 99, 132, 0.2)',
-  'rgba(54, 162, 235, 0.2)',
-  'rgba(255, 206, 86, 0.2)',
-]
-const borderColor_myChart = [
-  'rgba(255,99,132,1)',
-  'rgba(54, 162, 235, 1)',
-  'rgba(255, 206, 86, 1)',
-]
 
 const myChart = new Chart(ctx, {
   type: type_myChart,
-  data: {
-    labels: labels_myChart,
-    datasets: [{
-      fill: false,
-      label: label_myChart,
-      data: data_myChart,
-      backgroundColor: backgroundColor_myChart,
-      borderColor: borderColor_myChart,
-      borderWidth: 1
-    }]
-  },
-  options: {
-    scales: {
-      yAxes: [{
-        ticks: {
-          beginAtZero: true
-        }
-      }]
-    }
-  }
+  data: data,
+  options: options
 });
--- polar_area/chart.js
+++ polar_area/chart.js
@@ -1,44 +1,43 @@
 const ctx = document.getElementById("myChart").getContext('2d');
-const type_myChart = 'polarArea'
-const labels_myChart = ["IRIS", "ZDC", "Département", "France"]
-const data_myChart = [44, 49, 76, 69]
-const label_myChart = 'Densité de MG / 100.000 habitants'
 
-// console.log(ctx)
+const type_myChart = 'polarArea'
+
+const data = {
+  labels: ["IRIS", "ZDC", "Département", "France"],
+  datasets: [{
+    label: 'Densité de MG / 100.000 habitants',
+    data: [44, 49, 76, 69],
+    backgroundColor: [
+      'rgba(255, 99, 132, 0.2)',
+      'rgba(54, 162, 235, 0.2)',
+      'rgba(255, 206, 86, 0.2)',
+      'rgba(75, 192, 192, 0.2)'
+    ],
+    borderColor: [
+      'rgba(255,99,132,1)',
+      'rgba(54, 162, 235, 1)',
+      'rgba(255, 206, 86, 1)',
+      'rgba(75, 192, 192, 1)'
+    ],
+    borderWidth: 1
+  }]
+}
+
+const option = {
+  scales: {
+    yAxes: [{
+      ticks: {
+        beginAtZero: true
+      }
+    }]
+  }
+}
+//  console.log(ctx)
 // ctx.canvas.height = 100
 
-const backgroundColor_myChart = [
-  'rgba(255, 99, 132, 0.2)',
-  'rgba(54, 162, 235, 0.2)',
-  'rgba(255, 206, 86, 0.2)',
-  'rgba(75, 192, 192, 0.2)'
-]
-const borderColor_myChart = [
-  'rgba(255,99,132,1)',
-  'rgba(54, 162, 235, 1)',
-  'rgba(255, 206, 86, 1)',
-  'rgba(75, 192, 192, 1)'
-]
 
 const myChart = new Chart(ctx, {
   type: type_myChart,
-  data: {
-    labels: labels_myChart,
-    datasets: [{
-      label: label_myChart,
-      data: data_myChart,
-      backgroundColor: backgroundColor_myChart,
-      borderColor: borderColor_myChart,
-      borderWidth: 1
-    }]
-  },
-  options: {
-    scales: {
-      yAxes: [{
-        ticks: {
-          beginAtZero: true
-        }
-      }]
-    }
-  }
+  data: data,
+  options: option
 });
